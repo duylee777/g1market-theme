45b712c1a504bf2bdbfc3c227600cff6cee005f4
kizorry <duyleit98@gmail.com>
2023-10-24 14:57:14 +0700

    add assets and index file

---
+++ assets/js/style.js
@@ -0,0 +1,14 @@
+var btnShow = document.getElementsByClassName('mobile__bars')[0];
+var btnClose = document.getElementsByClassName('mobile__btn-close')[0];
+
+var mobileBox = document.getElementsByClassName('mobile-box')[0];
+
+btnClose.addEventListener('click', function() {
+    mobileBox.classList.remove('mobile-box--show')
+    mobileBox.classList.add('mobile-box--close');
+})
+
+btnShow.addEventListener('click', function() {
+    mobileBox.classList.remove('mobile-box--close')
+    mobileBox.classList.add('mobile-box--show')
+})
